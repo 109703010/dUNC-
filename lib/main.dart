f2cbb4581e75110c84b36cd081596b645be96582
109703010 <andyszztzn@gmail.com>
2022-05-06 09:47:57 +0800

    Fix the toggle and search bar. Add two elements in search page. Index of the toggle is global, should be modified.

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 
 const purple = Color(0xffaf57dc);
 const blue = Color(0xff7b78e2);
+const darkPurple = Color.fromARGB(255, 209, 169, 227);
 
 // const Map<int, Color> colorPurple = {
 //   50: Color.fromRGBO(56, 41, 87, .1),
@@ -43,107 +44,146 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+int selectedIndex = 0;
 
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(  // 為了避開螢幕的最上方
-            height: 50,
-          ),
-
-          Neumorphic(
-            style: NeumorphicStyle(
-              color: Colors.white,
-              shape: NeumorphicShape.flat,
-              depth: -20,
-              intensity: 1,
-              surfaceIntensity: 0,
-              boxShape: NeumorphicBoxShape.roundRect(const BorderRadius.all(Radius.circular(20))),
-            ),
-            child: NeumorphicToggle(  // 「找球隊/球員、找比賽」
+        body: Center(
+            child: Column(children: [
+      const SizedBox(
+        // 為了避開螢幕的最上方
+        height: 50,
+      ),
+      Neumorphic(
+        style: const NeumorphicStyle(
+            depth: -4,
+            surfaceIntensity: 0,
+            boxShape: NeumorphicBoxShape.stadium()),
+        child: SizedBox(
+            height: 45,
+            width: 250,
+            child: NeumorphicToggle(
+              movingCurve: Curves.easeInBack,
+              duration: const Duration(milliseconds: 600),
+              selectedIndex: selectedIndex,
+              onChanged: (int index) {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
               children: [
                 ToggleElement(
-                  foreground: Center(child: NeumorphicText(
-                    '找球隊/球員',
-                    style: const NeumorphicStyle(color: Colors.white),
-                  ),),
-                  background: Center(child: NeumorphicText(
-                    '找球隊/球員',
-                    style: const NeumorphicStyle(color: Colors.black),
-                  ),),
-                ),
-                ToggleElement(
-                  foreground: Center(child: NeumorphicText(
-                    '找比賽',
-                    style: const NeumorphicStyle(color: Colors.white),
-                  ),),
-                  background: Center(child: NeumorphicText(
-                    '找比賽',
-                    style: const NeumorphicStyle(color: Colors.black),
-                  ),)
-                )
-              ],
-
-              thumb: Container(  // 已被選擇的項目的樣式
-                foregroundDecoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.bottomLeft,
-                    end: Alignment.topRight,
-                    colors: [blue, purple]
+                  foreground: const Center(
+                    child: Text(
+                      '找球隊/球員',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  background: const Center(
+                    child: Text(
+                      '找球隊/球員',
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ),
+                ToggleElement(
+                    foreground: const Center(
+                      child: Text(
+                        '找比賽',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    background: const Center(
+                      child: Text(
+                        '找比賽',
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ))
+              ],
+              thumb: Container(
+                // 已被選擇的項目的樣式
+                foregroundDecoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.bottomLeft,
+                      end: Alignment.topRight,
+                      colors: [blue, purple]),
+                ),
               ),
-
               style: const NeumorphicToggleStyle(
-                backgroundColor: Colors.white54,  // 沒被選擇的項目背景顏色
+                backgroundColor: Colors.white54, // 沒被選擇的項目背景顏色
                 lightSource: LightSource(-0.6, -0.6),
-                borderRadius: BorderRadius.all(Radius.circular(20)),  // 選中項目
+                borderRadius: BorderRadius.all(Radius.circular(20)), // 選中項目
               ),
-            )
-          ),
-          const SizedBox(height: 25,),
+            )),
+      ),
+      const SizedBox(
+        height: 25,
+      ),
+      Neumorphic(
+        style: const NeumorphicStyle(
+            shadowLightColorEmboss: Colors.white,
+            depth: -4,
+            intensity: 0.75,
+            boxShape: NeumorphicBoxShape.stadium()),
+        child: const SizedBox(
+            height: 50,
+            width: 300,
+            child: TextField(
+                style: TextStyle(fontSize: 20),
+                maxLines: 1,
+                minLines: null,
+                decoration: InputDecoration(
+                    hintStyle: TextStyle(color: Colors.white),
+                    hintText: "輸入球隊/員",
+                    prefixIcon: Icon(
+                      Icons.search,
+                      size: 25,
+                      color: Colors.grey,
+                    )))),
+      ),
+      const SizedBox(height: 20),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
           Neumorphic(
-            style: NeumorphicStyle(
-              color: Colors.white,
-              shape: NeumorphicShape.flat,
-              depth: -20,
-              intensity: 1,
-              surfaceIntensity: 0,
-              boxShape: NeumorphicBoxShape.roundRect(const BorderRadius.all(Radius.circular(20))),
-            ),
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              child: LayoutBuilder(
-                builder: (BuildContext context, BoxConstraints constraints) {
-                  return SizedBox(
-                    width: constraints.maxWidth,
-                    child: constraints.maxWidth == double.infinity
-                        ? null
-                        : Row(
-                          children: [
-                            const Icon(
-                              Icons.search,
-                              color: Colors.grey,
-                            ),
-                            // TextField(
-                            //   decoration: null,
-                            //   keyboardType: TextInputType.name,
-                            //   textInputAction: TextInputAction.search,
-                            //   autofocus: true,
-                            //   onChanged: (str) {}, //todo
-                            // )
-                          ],
-                        )
-                  );
-                }
-              )
-            ),
-          )
+            style: const NeumorphicStyle(color: darkPurple),
+            child: const SizedBox(
+                height: 25,
+                width: 70,
+                child: Text(
+                  "搜尋過",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
+                )),
+          ),
+          const SizedBox(width: 30),
+          Neumorphic(
+            style: const NeumorphicStyle(color: darkPurple),
+            child: const SizedBox(
+                height: 25,
+                width: 110,
+                child: Text(
+                  "已加入我的最愛",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
+                )),
+          ),
         ],
       ),
-    );
+    ])));
   }
 }
